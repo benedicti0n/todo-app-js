c3872daef9d1dedc047aed4a9f0a632da3a52129
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-02-13 21:17:13 +0530

    value extracting done

---
+++ script.js
@@ -0,0 +1,10 @@
+const title = document.getElementById('title');
+const description = document.getElementById('description');
+const mainSection = document.getElementById('main-section');
+
+function onAdd(){
+    const titleValue = title.value;
+    const descriptionValue = description.value;
+
+
+}
